b0a75ed587f0d6593b322cc703ca3da0c96847b8
angelrdgz.sist@gmail.com <bicicleteadaporlapazsanluis@gmail.com>
2025-09-23 11:51:32 -0300

    video fix

--- components/video-section.tsx
+++ components/video-section.tsx
@@ -13,7 +13,7 @@ export function VideoSection() {
         <div className="relative w-full" style={{ paddingTop: "56.25%" }}>
           <iframe
             className="absolute top-0 left-0 w-full h-full rounded-lg shadow-lg"
-            src="https://www.youtube.com/embed/RqWmfspkztM?vq=hd720&rel=0&autoplay=1&mute=1&playsinline=1&loop=1&playlist=RqWmfspkztM"
+            src="https://www.youtube.com/embed/RqWmfspkztM?rel=0&autoplay=1&mute=1&playsinline=1&loop=1&playlist=RqWmfspkztM"
             title="Bicicleteada por la Paz 2024"
             allow="autoplay; loop; clipboard-write; encrypted-media; picture-in-picture; accelerometer; gyroscope"
             allowFullScreen
